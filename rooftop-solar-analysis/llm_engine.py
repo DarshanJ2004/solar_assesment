ffe6f73d2fda13b728048ed6b7a9aaeb5457769a
DARSHAN JETHVA <88070531+DarshanJ2004@users.noreply.github.com>
2025-05-26 21:44:56 +0530

    Update llm_engine.py

--- rooftop-solar-analysis/llm_engine.py
+++ rooftop-solar-analysis/llm_engine.py
@@ -1,7 +1,10 @@
 import os
 import requests
+from dotenv import load_dotenv
+load_dotenv()
 
-OPENROUTER_API_KEY = "Ypur_OpenRouter_API_Key"  
+# Get API key from environment variable
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 def generate_report(prompt):
     url = "https://openrouter.ai/api/v1/chat/completions"
